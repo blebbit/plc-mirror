353fa1066680c0c63f852f22fc54633cf5f3d0c2
Tony Worm <tony@topcialsource.com>
2025-03-07 13:39:53 -0500

    bump slow sql one more time

--- pkg/db/client.go
+++ pkg/db/client.go
@@ -39,7 +39,7 @@ func GetClient(dbUrl string, ctx context.Context) (*gorm.DB, error) {
 		Conn: sqldb,
 	}), &gorm.Config{
 		Logger: gormzerolog.New(&logger.Config{
-			SlowThreshold:             2 * time.Second,
+			SlowThreshold:             3 * time.Second,
 			IgnoreRecordNotFoundError: true,
 		}, nil),
 	})
